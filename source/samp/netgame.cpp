Leonardo541/gtasa-anticheat-asi

commit 1fb12d3c192e01e8c9ceaf3f462c1b985175a580

Adjust exploit detection
- Time elapsed detection changed (2s to 5s)
- Teleport dist detection changed (25.0 to 15.0)
- Health diff detection added

source/samp/netgame.cpp
@@ -141,7 +141,7 @@ void CNetGame::Packet_VehicleSync(Packet *packet)
 											
 											time_t time_elapsed = rawtime - jacked.GetPtr()->GetRawtime();
 											
-											if(time_elapsed <= 2)
+											if(time_elapsed <= 5)
 											{
 												old_position = jacked.GetPtr()->GetOldPosition();
 												old_up_z = jacked.GetPtr()->GetOldUpZ();
@@ -166,7 +166,9 @@ void CNetGame::Packet_VehicleSync(Packet *packet)
 										}
 									}
 									
-									if(teleport_dist > 25.0)
+									float vehicle_health_diff = vehicle_health - old_health;
+									
+									if(teleport_dist > 15.0f)
 										show_alert = true;
 									
 									if(teleport_up_z <= 0.0f && old_up_z > 0.0f)
@@ -175,7 +177,10 @@ void CNetGame::Packet_VehicleSync(Packet *packet)
 									if(vehicle_health <= 250.0f && old_health > 250.0f)
 										show_alert = true;
 									
-									CRefPtr<CJacked> jacked = new CJacked(playerid, playername, driverid, drivername, teleport_dist, teleport_up_z, vehicle_health, (float)vehicle_health - old_health, old_position, old_up_z, old_health, counter, rawtime, show_alert);
+									if(fabs(vehicle_health_diff) > 15.0f)
+										show_alert = true;
+									
+									CRefPtr<CJacked> jacked = new CJacked(playerid, playername, driverid, drivername, teleport_dist, teleport_up_z, vehicle_health, vehicle_health_diff, old_position, old_up_z, old_health, counter, rawtime, show_alert);
 									
 									jacked_list.push_back(jacked);
 									

commit 68cc2bf2153ca9f17808697eaf5d082f266a95bf

Fix false positives detection
Fixes: 1fb12d3

The initial idea was to detect changes of 15% (150 hp) in the health difference. However, it was mistakenly set at 1.5% (15 hp).

The value has now been increased to 20% (200 hp) and only for negative values.

Changes:
- Teleport dist detection changed (15.0 to 20.0)
- Health diff detection changed (15.0 to 200.0)

source/samp/netgame.cpp
@@ -177,7 +177,7 @@ void CNetGame::Packet_VehicleSync(Packet *packet)
 									
 									float vehicle_health_diff = vehicle_health - old_health;
 									
-									if(teleport_dist > 15.0f)
+									if(teleport_dist > 20.0f)
 										show_alert = true;
 									
 									if(teleport_up_z <= 0.0f && old_up_z > 0.0f)
@@ -186,7 +186,7 @@ void CNetGame::Packet_VehicleSync(Packet *packet)
 									if(vehicle_health <= 250.0f && old_health > 250.0f)
 										show_alert = true;
 									
-									if(fabs(vehicle_health_diff) > 15.0f)
+									if(vehicle_health_diff < -200.0f)
 										show_alert = true;
 									
 									CRefPtr<CJacked> jacked = new CJacked(playerid, playername, driverid, drivername, teleport_dist, teleport_up_z, vehicle_health, vehicle_health_diff, old_position, old_up_z, old_health, counter, rawtime, show_alert);